49e402c4c234567970bc8b200602246e76703c18
madhavbiradar02 <madhav18biradar@gmail.com>
2025-04-01 20:32:25 +0530

    Update terr.tf
    EC2-Code

--- terr.tf
+++ terr.tf
@@ -1,3 +1,12 @@
-Hello 
+provider "aws" {
+  region = "us-east-1"
+}
 
-this is first terrform code 
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Use a valid AMI ID in your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
